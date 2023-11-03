0008591b0aedd649aabe00a1be487f3c77098be2
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-11-04 03:56:23 +0800

    Format program index file

--- resources/views/programs/index.blade.php
+++ resources/views/programs/index.blade.php
@@ -4,7 +4,8 @@
         <div class="container py-5">
             <div class="text-center">
                 <h1 class="display-4 fw-bold text-dark">Choose your course programme</h1>
-                <p class="mt-4 lead text-secondary">Explore job scope and roles based on your chosen course programme.</p>
+                <p class="mt-4 lead text-secondary">Explore job scope and roles based on your chosen course programme.
+                </p>
             </div>
 
             <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-3 g-4 mt-4">
@@ -12,7 +13,7 @@
                     <div class="bg-white p-4 rounded shadow">
                         <h2 class="h4 fw-bold text-dark">Software Engineer</h2>
                         <p class="mt-3 text-secondary">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        <a href="{{ route('job.list')}}" class="btn btn-light custom-button">Explore</a>
+                        <a href="{{ route('job.list') }}" class="btn btn-light custom-button">Explore</a>
                     </div>
                 </div>
 
@@ -20,7 +21,7 @@
                     <div class="bg-white p-4 rounded shadow">
                         <h2 class="h4 fw-bold text-dark">Data and Network</h2>
                         <p class="mt-3 text-secondary">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        <a href="{{ route('job.list')}}" class="btn btn-light custom-button">Explore</a>
+                        <a href="{{ route('job.list') }}" class="btn btn-light custom-button">Explore</a>
                     </div>
                 </div>
 
@@ -28,7 +29,7 @@
                     <div class="bg-white p-4 rounded shadow">
                         <h2 class="h4 fw-bold text-dark">Graphic and Multimedia</h2>
                         <p class="mt-3 text-secondary">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        <a href="{{ route('job.list')}}" class="btn btn-light custom-button">Explore</a>
+                        <a href="{{ route('job.list') }}" class="btn btn-light custom-button">Explore</a>
                     </div>
                 </div>
             </div>
@@ -41,7 +42,7 @@
                 <h1 class="text-4xl font-bold text-gray-800">Welcome to Our Website</h1>
                 <p class="mt-4 text-lg text-gray-600">Explore our amazing features and services.</p>
             </div>
-            
+
             <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-12">
                 <div class="bg-white p-6 rounded-lg shadow-md">
                     <h2 class="text-xl font-semibold text-gray-800">Software Engineer</h2>
@@ -56,7 +57,7 @@
                 <div class="bg-white p-6 rounded-lg shadow-md">
                     <h2 class="text-xl font-semibold text-gray-800">Feature 3</h2>
                     <p class="mt-2 text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    <button type="button" class="btn btn-primary">Explore</button>  
+                    <button type="button" class="btn btn-primary">Explore</button>
                 </div>
             </div>
         </div>
